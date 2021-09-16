c7629765976724c78c62814e3f427d138b1736c2
Rohit Kesarwani <rkesarwa@localhost.localdomain>
2021-09-17 04:51:07 +0530

    update openssl to 1.1.1l

--- NVPTest/compile.sh
+++ NVPTest/compile.sh
@@ -8,6 +8,6 @@ echo '  Compiling...'
 g++  -Wall -g -c -I../NVPClient -I../BaseClient NVPTest.cpp -o NVPTest.o
 
 echo '  Linking...'
-g++ -g -o NVPTest NVPTest.o -L../BaseClient -L../NVPClient -lbaseclient -lNVPClient -Wl,--no-as-needed -ldl -Wl,-rpath,'$ORIGIN/../lib/openssl-1.1.1k/linux64/lib' -L../lib/openssl-1.1.1k/linux64/lib -l:libssl.so.1.1 -l:libcrypto.so.1.1
+g++ -g -o NVPTest NVPTest.o -L../BaseClient -L../NVPClient -lbaseclient -lNVPClient -Wl,--no-as-needed -ldl -Wl,-rpath,'$ORIGIN/../lib/openssl-1.1.1l/linux64/lib' -L../lib/openssl-1.1.1l/linux64/lib -l:libssl.so.1.1 -l:libcrypto.so.1.1
 echo '  Done.'
 
